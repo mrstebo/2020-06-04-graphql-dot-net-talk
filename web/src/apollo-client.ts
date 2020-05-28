df4c37a85d7bb38ed0d8b8d63b3e7af9d3796e2a
Steven Atkinson <mrstebouk@gmail.com>
2020-05-28 16:32:39 +0100

    Updated the apollo-client to enable subscriptions

--- web/src/apollo-client.ts
+++ web/src/apollo-client.ts
@@ -1,7 +1,36 @@
-import ApolloClient from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost/api',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost',
+  options: {
+    reconnect: true,
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: '/api',
+  cache: new InMemoryCache(),
+  link,
 });
 
 export default client;
