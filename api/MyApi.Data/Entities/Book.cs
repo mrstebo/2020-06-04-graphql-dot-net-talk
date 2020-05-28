87a099c1a9a3d84391af7491f1354393d900496b
Steven Atkinson <mrstebouk@gmail.com>
2020-05-28 11:27:34 +0100

    Added the Description field to the Book model

--- api/MyApi.Data/Entities/Book.cs
+++ api/MyApi.Data/Entities/Book.cs
@@ -5,6 +5,7 @@ namespace MyApi.Data.Entities
         public long Id { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
+        public string Description { get; set; }
 
         public long AuthorId { get; set; }
         public virtual Author Author { get; set; }
